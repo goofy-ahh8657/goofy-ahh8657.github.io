0c655a5e7a90fe4247ff434af61e9126a001da29
goofy-ahh8657 <150096483+goofy-ahh8657@users.noreply.github.com>
2024-09-04 06:37:55 -0400

    Update subtitle.js

--- js/subtitle.js
+++ js/subtitle.js
@@ -10,13 +10,27 @@ var say = [
   "the floor is made of floor",
   "breathing air helps you stay alive",
   "welp, i tried :)",
-  "made by your dad's mom's dad's mom's dad's mom",
+  "sponsored by your dad's mom's dad's mom's dad's mom",
   "gedagedigedagedago abin mery alontamago",
   "technically good",
   "khaaaaaaaaaaaaaaaaaaan",
   "random splash text",
   "played by cowboys",
-  "gluten-free"
+  "gluten-free",
+  "i'm slim shady, yes i'm slim shady",
+  "may contain nuts",
+  "pov that one person who's 2 braincells are fighting for second place"
+  "bada ba ba ba",
+  "searching for nearby sushi restauraunts...",
+  "site shutting down in two days...jk",
+  "no donkeys were harmed in the making of this site",
+  "warning: brain cells may be lost if you proceed",
+  "downloading your passwords now...",
+  "he on x games mode",
+  "light mode eww",
+  "99.9% bug free",
+  "now with crappy games",
+  "fbi open up",
 ];
 
 
